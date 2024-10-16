6167d8ac5052466dcbd5a094d2ee5d9d70b2ffb3
fabriciarosarodriues <fabricia.rosa.rodrigues00@gmail.com>
2024-10-16 13:25:47 +0200

    Add files via upload

---
+++ ps1.ps1
@@ -0,0 +1,5 @@
+﻿New-Item -Path "C:\" -Name "Prueba" -ItemType "directory"
+Set-Location -Path C:\prueba 
+New-Item -Path . -Name Hola3.txt -ItemType "file" -Value "Jairo es Gay"
+Copy-Item -Path "C:\prueba\Hola3.txt" -Destination "C:\prueba\Hola3.bak"
+
